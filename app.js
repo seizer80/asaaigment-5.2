73d811d9646d0568146b7ec5e98b922e5dbcf76c
kangmin <seizer80@gmail.com>
2023-06-06 14:23:41 -0700

    firstone

---
+++ app.js
@@ -0,0 +1,44 @@
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
+// const panagram = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'];
+
+// // console.log(nums.every(num => num >= 0));
+
+// // console.log(panagram.every(word => word.length <= 8));
+
+
+// // console.log(nums.filter(num => num < 4));
+// // console.log(panagram.filter(word => word.length % 2 === 0));
+
+// // console.log(nums.find(num => num % 5 === 0));
+// // console.log(panagram.find(word => word.length > 5));
+
+// // console.log(nums.findIndex(num => num % 3 === 0));
+// // console.log(panagram.findIndex(word => word.length < 2));
+
+
+// // nums.forEach(num => console.log(num * 3));
+// // panagram.forEach(word => console.log(word + "!"));
+
+// // const newArray = nums.map(num => num * 100);
+// // console.log(newArray)
+
+// // const newPanagram = panagram.map(word => word.toUpperCase());
+// // console.log(newPanagram);
+
+// // console.log(nums.some(num => num % 7 === 0));
+// // console.log(panagram.some(word => word.includes('a')));
+
+
+
+// console.log(nums.reduce((a, b) => a + b));
+// console.log(panagram.reduce((a, b) => a + b));
+
+
+
+// console.log(nums.sort());
+// console.log(panagram.sort());
+// console.log(nums.sort((a, b) => a - b));
+// console.log(nums.sort((a, b) => b - a));
+// console.log(panagram.sort((a, b) => a.localeCompare(b)));
+// console.log(panagram.sort((a, b) => b.localeCompare(a)));
+
